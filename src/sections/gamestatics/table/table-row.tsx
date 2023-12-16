9af486cfa109e03f5a09095279cc72a950fbb6a6
hardwork0612 <mateomuhannad@gmail.com>
2023-12-16 14:56:27 +0000

    [Update] game statics

--- src/sections/gamestatics/table/table-row.tsx
+++ src/sections/gamestatics/table/table-row.tsx
@@ -1,10 +1,9 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 // @mui
 import Box from '@mui/material/Box';
 // import Paper from '@mui/material/Paper';
 // import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
-import Collapse from '@mui/material/Collapse';
 import TableRow from '@mui/material/TableRow';
 import Checkbox from '@mui/material/Checkbox';
 import TableCell from '@mui/material/TableCell';
@@ -18,7 +17,6 @@ import { fcustomCurrency } from 'src/utils/format-number';
 
 // components
 import Iconify from 'src/components/iconify';
-import { Paper, Stack, Table, TableBody } from '@mui/material';
 
 // ----------------------------------------------------------------------
 
@@ -37,143 +35,89 @@ export default function OrderTableRow({ row, selected, onViewRow, onSelectRow, o
 
     const collapse = useBoolean();
 
-    const [toggleValue, setToggleValue] = useState<any>({});
-    const ontoggle = (id: string, item: any) => {
-        console.log(item);
-        const ids: any = get_collape_ids(item);
-        setToggleValue((prev: any) => {
-            let item_: any = {};
-            if (prev[id]) {
-                item_ = {
-                    ...prev,
-                    [id]: !prev[id],
-                    ...ids
-                };
-            } else {
-                item_ = {
-                    ...prev,
-                    [id]: !prev[id]
-                };
-            }
-            return item_;
-        });
-        // setToggleValue((prev: any) => ({
-        // }
-        // ));
-    };
+    const [opendList, setOpendList] = useState<any>({});
 
-    const get_collape_ids = (item: any) => {
-        const ids: any = {};
-        function fact(list: any) {
-            for (let i = 0; i < list.length; i += 1) {
-                if (list[i].children && list[i].children.length) {
-                    ids[list[i].id] = false;
-                    fact(list[i].children);
-                }
-            }
+    const handleOpenChild = (id: string) => {
+        if (opendList[id]) {
+            setOpendList((pre: any) => ({ ...pre, [id]: false }));
+        } else {
+            setOpendList((pre: any) => ({ ...pre, [id]: true }));
         }
-        fact([item]);
-        return ids;
     };
 
     const renderSecondary = (childrenItem: any) => (
         <>
             {childrenItem.children.map((item: any, key: any) => (
-                <Fragment key={key}>
-                    {item.outamount !== undefined && (
-                        <>
-                            <TableRow>
-                                <TableCell sx={{ p: 0, border: 'none' }} colSpan={8}>
-                                    <Collapse
-                                        in={toggleValue[item.parent_id]}
-                                        timeout="auto"
-                                        unmountOnExit
-                                        sx={{ bgcolor: 'background.neutral' }}
-                                    >
-                                        <Stack component={Paper} sx={{ m: 1.5 }}>
-                                            <Table size="small" sx={{ minWidth: 960 }}>
-                                                <TableBody>
-                                                    <TableRow hover selected={selected}>
-                                                        <TableCell padding="checkbox">
-                                                            <Checkbox checked={selected} onClick={onSelectRow} />
-                                                        </TableCell>
+                <>
+                    <TableRow hover selected={selected} key={key}>
+                        <TableCell padding="checkbox">
+                            <Checkbox checked={selected} onClick={onSelectRow} />
+                        </TableCell>
 
-                                                        <TableCell sx={{ width: 352 }}>
-                                                            <Box
-                                                                onClick={onViewRow}
-                                                                sx={{
-                                                                    cursor: 'pointer',
-                                                                    '&:hover': {
-                                                                        textDecoration: 'underline'
-                                                                    }
-                                                                }}
-                                                            >
-                                                                {item._id}
-                                                                {/* {orderNumber} */}
-                                                            </Box>
-                                                        </TableCell>
+                        <TableCell sx={{ width: 352 }}>
+                            <Box
+                                onClick={onViewRow}
+                                sx={{
+                                    cursor: 'pointer',
+                                    '&:hover': {
+                                        textDecoration: 'underline'
+                                    }
+                                }}
+                            >
+                                {item._id}
+                            </Box>
+                        </TableCell>
 
-                                                        <TableCell
-                                                            sx={{
-                                                                display: 'flex',
-                                                                minWidth: 270
-                                                            }}
-                                                        >
-                                                            <Avatar alt={username} src={avatar} sx={{ mr: 2 }} />
+                        <TableCell
+                            sx={{
+                                display: 'flex',
+                                width: 270
+                            }}
+                        >
+                            <Avatar alt={username} src={avatar} sx={{ mr: 2 }} />
 
-                                                            <ListItemText
-                                                                primary={item.username}
-                                                                secondary={item.email}
-                                                                primaryTypographyProps={{ typography: 'body2' }}
-                                                                secondaryTypographyProps={{
-                                                                    component: 'span',
-                                                                    color: 'text.disabled'
-                                                                }}
-                                                            />
-                                                        </TableCell>
+                            <ListItemText
+                                primary={item.username}
+                                secondary={item.email}
+                                primaryTypographyProps={{ typography: 'body2' }}
+                                secondaryTypographyProps={{
+                                    component: 'span',
+                                    color: 'text.disabled'
+                                }}
+                            />
+                        </TableCell>
 
-                                                        <TableCell sx={{ width: mdUp ? 120 : 140 }}>
-                                                            {fcustomCurrency(item.outamount)}
-                                                        </TableCell>
+                        <TableCell sx={{ width: mdUp ? 120 : 140 }}>{fcustomCurrency(item.outamount)}</TableCell>
 
-                                                        <TableCell sx={{ width: mdUp ? 120 : 140 }}>
-                                                            {fcustomCurrency(item.inamount)}
-                                                        </TableCell>
+                        <TableCell sx={{ width: mdUp ? 120 : 140 }}>{fcustomCurrency(item.inamount)}</TableCell>
 
-                                                        <TableCell sx={{ width: mdUp ? 120 : 140 }}>
-                                                            {fcustomCurrency(item.outamount - item.inamount)}
-                                                        </TableCell>
-                                                        {item.children.length > 0 ? (
-                                                            <TableCell
-                                                                align="right"
-                                                                sx={{ px: 1, whiteSpace: 'nowrap', width: 70 }}
-                                                            >
-                                                                <IconButton
-                                                                    color={collapse.value ? 'inherit' : 'default'}
-                                                                    onClick={() => ontoggle(item.id, item)}
-                                                                    sx={{
-                                                                        ...(collapse.value && {
-                                                                            bgcolor: 'action.hover'
-                                                                        })
-                                                                    }}
-                                                                >
-                                                                    <Iconify icon="eva:arrow-ios-downward-fill" />
-                                                                </IconButton>
-                                                            </TableCell>
-                                                        ) : (
-                                                            <TableCell sx={{ width: 70 }} />
-                                                        )}
-                                                    </TableRow>
-                                                </TableBody>
-                                            </Table>
-                                        </Stack>
-                                    </Collapse>
-                                </TableCell>
-                            </TableRow>
-                            {item.children.length > 0 && renderSecondary(item)}
-                        </>
-                    )}
-                </Fragment>
+                        <TableCell sx={{ width: mdUp ? 120 : 140 }}>
+                            {fcustomCurrency(item.outamount - item.inamount)}
+                        </TableCell>
+                        {item.children.length > 0 ? (
+                            <TableCell align="right" sx={{ px: 1, whiteSpace: 'nowrap', width: 70 }}>
+                                <IconButton
+                                    color={collapse.value ? 'inherit' : 'default'}
+                                    onClick={() => handleOpenChild(item.id)}
+                                    sx={{
+                                        ...(collapse.value && {
+                                            bgcolor: 'action.hover'
+                                        })
+                                    }}
+                                >
+                                    {opendList[item.id] ? (
+                                        <Iconify icon="eva:arrow-ios-upward-fill" />
+                                    ) : (
+                                        <Iconify icon="eva:arrow-ios-downward-fill" />
+                                    )}
+                                </IconButton>
+                            </TableCell>
+                        ) : (
+                            <TableCell sx={{ width: 70 }} />
+                        )}
+                    </TableRow>
+                    {opendList[item.id] && renderSecondary(item)}
+                </>
             ))}
         </>
     );
@@ -187,7 +131,7 @@ export default function OrderTableRow({ row, selected, onViewRow, onSelectRow, o
                     <Checkbox checked={selected} onClick={onSelectRow} />
                 </TableCell>
 
-                <TableCell>
+                <TableCell sx={{ width: 352 }}>
                     <Box
                         onClick={onViewRow}
                         sx={{
@@ -201,7 +145,7 @@ export default function OrderTableRow({ row, selected, onViewRow, onSelectRow, o
                     </Box>
                 </TableCell>
 
-                <TableCell sx={{ display: 'flex', alignItems: 'center' }}>
+                <TableCell sx={{ display: 'flex', alignItems: 'center', width: 270 }}>
                     <Avatar alt={username} src={avatar} sx={{ mr: 2 }} />
 
                     <ListItemText
@@ -215,30 +159,34 @@ export default function OrderTableRow({ row, selected, onViewRow, onSelectRow, o
                     />
                 </TableCell>
 
-                <TableCell> {fcustomCurrency(outamount)} </TableCell>
+                <TableCell sx={{ width: mdUp ? 120 : 140 }}> {fcustomCurrency(outamount)} </TableCell>
 
-                <TableCell>{fcustomCurrency(inamount)}</TableCell>
+                <TableCell sx={{ width: mdUp ? 120 : 140 }}>{fcustomCurrency(inamount)}</TableCell>
 
-                <TableCell> {fcustomCurrency(outamount - inamount)} </TableCell>
+                <TableCell sx={{ width: mdUp ? 120 : 140 }}> {fcustomCurrency(outamount - inamount)} </TableCell>
                 {row.children.length > 0 ? (
-                    <TableCell align="right" sx={{ px: 1, whiteSpace: 'nowrap' }}>
+                    <TableCell align="right" sx={{ px: 1, whiteSpace: 'nowrap', width: 70 }}>
                         <IconButton
                             color={collapse.value ? 'inherit' : 'default'}
-                            onClick={() => ontoggle(row.id, row)}
+                            onClick={() => handleOpenChild(row.id)}
                             sx={{
                                 ...(collapse.value && {
                                     bgcolor: 'action.hover'
                                 })
                             }}
                         >
-                            <Iconify icon="eva:arrow-ios-downward-fill" />
+                            {opendList[row.id] ? (
+                                <Iconify icon="eva:arrow-ios-upward-fill" />
+                            ) : (
+                                <Iconify icon="eva:arrow-ios-downward-fill" />
+                            )}
                         </IconButton>
                     </TableCell>
                 ) : (
-                    <TableCell />
+                    <TableCell sx={{ width: 70 }} />
                 )}
             </TableRow>
-            {row.children.length > 0 && renderSecondary(row)}
+            {opendList[row.id] && renderSecondary(row)}
         </>
     );
 }
